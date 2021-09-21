ba6ec5582d233463d221bbf71bc43032a6cf6893
Jona <mr-jona@hotmail.com>
2021-09-21 18:46:57 +0200

    make it possible to add characters in the display

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 };
+
+
+
+let display = document.querySelector('#display');
+
+
+
+
+window.addEventListener('click', function(e) {
+    let input = document.querySelector(`button[id="${e.target.id}"]`);
+    
+    console.log(input);
+    if (!input) return;
+
+    if (input.id === 'clearButton') return; // clearCalculator()
+
+    if (input.id === 'equalButton') return; //return whatever will be the function that calculates stuff
+    
+    display.textContent += input.textContent;
+});
+
+
+//function that will clear the display
+// function clearCalculator() {
+//  };
