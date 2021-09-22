4135d2b45399453f5f8e80c05ef3f68b5143c614
Jona <mr-jona@hotmail.com>
2021-09-22 16:47:49 +0200

    make the calculator work roughly, lots of bugs still.

--- script.js
+++ script.js
@@ -1,46 +1,103 @@
-function add(a, b) {
-    return a + b;
-};
-
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-};
-
-
-
-function operate(operator, a, b) {
-    return operator(a, b);
-};
-
-
-
 let display = document.querySelector('#display');
+let displayValue = "";
+let firstNumber = 0;
+let selectedOperator = "p";
+let secondNumber = 0;
 
 
+window.addEventListener('click', function(e) { //populates the display
 
-
-window.addEventListener('click', function(e) {
     let input = document.querySelector(`button[id="${e.target.id}"]`);
     
-    console.log(input);
+    if (display.textContent.length > 15) return;
+
     if (!input) return;
-
-    if (input.id === 'clearButton') return; // clearCalculator()
-
-    if (input.id === 'equalButton') return; //return whatever will be the function that calculates stuff
     
+    if (input.id === 'clearButton') return clearCalculator();
+    
+    if (input.id === 'equalButton') return; //return whatever will be the function that calculates stuff
+
     display.textContent += input.textContent;
+    
+    displayValue = display.textContent;
 });
 
 
-//function that will clear the display
-// function clearCalculator() {
-//  };
+
+function clearCalculator() {
+    display.textContent = "";
+};
+
+
+
+
+
+let operatorSign = "";
+
+window.addEventListener('click', function(e) { //asigns the inputted values to the corresponding variables
+
+    let input = document.querySelector(`button[id="${e.target.id}"]`);
+
+    if (input.className === 'operator') {
+        selectedOperator = input.id;
+
+        operatorSign = input.textContent;
+
+    };
+
+    if (input.id === 'equalButton') {
+        bothNumbers = displayValue.split(`${operatorSign}`);
+
+
+        firstNumber = bothNumbers[0];
+        secondNumber = bothNumbers[1];
+
+        
+        return operate();
+    };
+});
+
+
+let answer = "";
+
+function add(a, b) {
+    answer = a + b;
+    console.log(answer);
+    return display.textContent = answer;
+};
+
+function subtract(a, b) {
+    answer = a - b;
+    console.log(answer);
+    return display.textContent = answer;
+};
+
+function multiply(a, b) {
+    answer = a * b;
+    console.log(answer);
+    return display.textContent = answer;
+};
+
+function divide(a, b) {
+    answer = a / b;
+    console.log(answer);
+    return display.textContent = answer;
+};
+
+
+
+function operate() {
+    
+    console.log(selectedOperator);
+    console.log(firstNumber);
+    console.log(secondNumber);
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (selectedOperator === 'add') return add(firstNumber, secondNumber);
+    if (selectedOperator === 'subtract') return subtract(firstNumber, secondNumber);
+    if (selectedOperator === 'multiply') return multiply(firstNumber, secondNumber);
+    if (selectedOperator === 'divide') return divide(firstNumber, secondNumber);
+    
+};
